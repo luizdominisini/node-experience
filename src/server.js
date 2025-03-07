b81b450f5b15df5cad670f446b49c8a33eb4bf71
Luiz Gustavo Dominisini <dominizi.luiz@gmail.com>
2025-03-07 14:57:38 -0300

    perguntas e respotas

--- src/server.js
+++ src/server.js
@@ -2,10 +2,15 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.set("view engine", "ejs");
+
+app.get("/:nome", (req, res) => {
+  const nome = req.params.nome;
+  res.render("index.ejs", {nome});
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT} in adress http://localhost:${PORT}`);
+  console.log(
+    `Server is running on port ${PORT} in adress http://localhost:${PORT}`
+  );
 });
