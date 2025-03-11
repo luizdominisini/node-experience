27dbf7e9000171fde1816cf81c37ce0a16baf824
Luiz Gustavo Dominisini <dominizi.luiz@gmail.com>
2025-03-10 22:02:43 -0300

    iniciando novo estudo

---
+++ aula01-mocks/src/constants.js
@@ -0,0 +1,8 @@
+const constansts = {
+  error: {
+    FILE_LENGTH_ERROR_MESSAGE: "The content length is invalid!",
+    FILE_FIELDS_ERROR_MESSAGE: "The provided properties as invalid!",
+  },
+};
+
+module.exports = constansts;
